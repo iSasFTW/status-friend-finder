546544cd8e8e7a1a82d68a766f5846b2408f3f2b
iSasFTW <saska.kolehmainen@hotmail.com>
2017-01-08 21:08:46 +0200

    Added jumbotron, added firebase

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+          var config = {
+            apiKey: "AIzaSyAuGWK3aQiwgAZzkA4Z9NbqG6q8qJvr7AA",
+            authDomain: "status-friend-finder.firebaseapp.com",
+            databaseURL: "https://status-friend-finder.firebaseio.com",
+            storageBucket: "status-friend-finder.appspot.com",
+            messagingSenderId: "38615167502"
+          };
+          firebase.initializeApp(config);
+
